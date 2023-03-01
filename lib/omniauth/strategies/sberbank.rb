36c53784c36c7843c0dab66cafc29dcd8fbbb4c8
Danil Shulga <danil.shulga@insales.ru>
2023-03-01 13:05:05 +0300

    redirect url

--- lib/omniauth/strategies/sberbank.rb
+++ lib/omniauth/strategies/sberbank.rb
@@ -110,6 +110,12 @@ module OmniAuth
         }
       end
 
+      def callback_url
+        return options.redirect_url + query_string if options.redirect_url.present?
+
+        full_host + script_name + callback_path + query_string
+      end
+
       def query_string
         params = request.params.except('state', 'nonce', 'code')
         request.query_string.empty? ? '' : "?#{params.to_query}"
